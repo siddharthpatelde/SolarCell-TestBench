3f45baef87fb9b08a53e8c15631bbf7b16918b59
Siddharth Patel <141241898+siddharthpatelde@users.noreply.github.com>
2024-11-18 18:04:46 +0100

    test 1 for current sensor

--- Solar Cell Measurement System/ACS712_20A_test/ACS712_20A_test.ino
+++ Solar Cell Measurement System/ACS712_20A_test/ACS712_20A_test.ino
@@ -1,51 +1,17 @@
-/* 
- CREATED BY AATIK"s LAB
- FOR TESTING PURPOSE
-
-SENSOR CONNECTIONS
-
-GND_PIN -> GND OF ARDUINO
-VCC     -> 5v PIN OF ARDUINO
-OUT     -> A0 PIN OF ARDUNO
- 
- This code is for the 5A sensor, if 20A or 30A sensor.
- You need to MODIFY/REPLACE (.0264 * analogRead(A0) -13.51) this formula to 
- 
-(.19 * analogRead(A0) -25) for 20A sensor
-(.044 * analogRead(A0) -3.78) for 30A sensor
-*/
-
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
- 
-  float average = 0;
-  for(int i = 0; i < 1000; i++) 
-  {
-    average = average + (.19 * analogRead(A0) -13.51) / 1000;
-    delay(1);
+  // put your main code here, to run repeatedly:
+  int adc = analogRead(A0);
+  float voltage = adc * 5 / 1023.0;
+  float current = (voltage - 2.5) / 0.100; //for 20A 
+  if (current < 0.16) {
+    current = 0;
   }
-  Serial.println(average);  
+  Serial.print("Current : ");
+  Serial.println(current);
+  delay(300);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
