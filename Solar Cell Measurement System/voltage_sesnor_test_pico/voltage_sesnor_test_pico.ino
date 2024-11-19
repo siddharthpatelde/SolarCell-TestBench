c17c8f0b8e310ea1adc65af8339f87473c87156b
Siddharth Patel <141241898+siddharthpatelde@users.noreply.github.com>
2024-11-19 13:16:44 +0100

    added voltage sesnsor test for pico

--- Solar Cell Measurement System/voltage_sesnor_test_pico/voltage_sesnor_test_pico.ino
+++ Solar Cell Measurement System/voltage_sesnor_test_pico/voltage_sesnor_test_pico.ino
@@ -1,20 +1,15 @@
-// This code is for Arduino Uno
+// This code is for Raspberry pi pico
 
 // Voltage measurement using a voltage sensor connected to pin A3
 
-//  v_in on adapter = 12.11V
-//  v_out on sensor = 2.43
-//  5V on Arduino v_cc = 5.06V
- 
-//  Vin / vOut = factor
-//  12.11 / 2.43 = 4.98
+//factor will be 3.3 for raspbery pi pico
 
 // Pin Definitions
-const int voltageSensorPin = A3;  // Voltage sensor input pin (green wire)
+const int voltageSensorPin = A1;  // Voltage sensor input pin (green wire)
 
 // Constants
-const float factor = 4.98;        // Reduction factor of the Voltage Sensor shield
-const float v_cc = 5.06;           // Arduino input voltage (measurable by voltmeter)
+const float factor = 3.3;        // Reduction factor of the Voltage Sensor shield
+const float v_cc = 4.98;           // Arduino input voltage (measurable by voltmeter)
 
 // Variables
 float v_in = 0.0;                  // Measured input voltage
@@ -31,7 +26,7 @@ void loop() {
   sesnor_value = analogRead(voltageSensorPin);
 
   // Convert the raw value to the actual voltage on the analog pin
-  v_out = (sesnor_value / 1024.0) * v_cc;
+  v_out = (sesnor_value / 1024.0 ) * v_cc;
 
   // Calculate the input voltage using the sensor factor
   v_in = v_out * factor;
